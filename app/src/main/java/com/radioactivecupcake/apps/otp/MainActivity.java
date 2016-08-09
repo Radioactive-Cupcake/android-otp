1698cddc99e8e73bbc27a493f5ddd1a88128fd03
Saxon Knight <saxon.knight.rc@gmail.com>
2016-08-08 22:50:48 -1000

    Update IDs and string resources

--- app/src/main/java/com/radioactivecupcake/apps/otp/MainActivity.java
+++ app/src/main/java/com/radioactivecupcake/apps/otp/MainActivity.java
@@ -80,9 +80,9 @@ public class MainActivity extends AppCompatActivity
         // Handle navigation view item clicks here.
         int id = item.getItemId();
 
-        if (id == R.id.nav_camera) {
+        if (id == R.id.nav_keys) {
             // Handle the camera action
-        } else if (id == R.id.nav_gallery) {
+        } else if (id == R.id.nav_crypt) {
 
         } else if (id == R.id.nav_slideshow) {
 
